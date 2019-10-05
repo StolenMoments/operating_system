b75750d89390a069e18dc9c8b3b6001217680679
LEEJONGHWA <unknown9732@naver.com>
2019-10-05 15:07:14 +0900

    Update 세마포어 예제.cpp

--- 세마포어 예제.cpp
+++ 세마포어 예제.cpp
@@ -1,60 +1,40 @@
 // reference : https://austingwalters.com/multithreading-semaphores
-// 입출금 요청이 여러개 들어올 때, 입금-출금-입금-출금-... 순서로 Ordering
-#include <iostream>      
-#include <thread>        
-#include <mutex>         
-#include <condition_variable>
+// 그냥 mtx.lock, mtx.unlock 으로 구현...
+#include <thread>
+#include <mutex>
+#include <iostream>
 using namespace std;
 
-mutex mtx;             // mutex for critical section
-condition_variable cv; // condition variable for critical section  
-bool ready = false;         // Tell threads to run
+mutex mtx;
+
+bool ready = false;
 int balance = 0;
-int num = 0;
-int value = 1;
 
 void depo(int cnt) {
-	unique_lock<mutex> lck(mtx);
-	while (ready) cv.wait(lck);
+	mtx.lock();
 	balance += 1000;
 	cout << cnt << " 번째 1000원 입금, 잔액 : " << balance << endl;
 	ready = true;
-	cv.notify_all();
+	mtx.unlock();
 }
 
 void draw(int cnt) {
-	unique_lock<mutex> lck(mtx);
-	while (!ready) cv.wait(lck);
+	mtx.lock();
 	if (balance >= 1000) {
 		balance -= 1000;
 		cout << cnt << " 번째 1000원 출금, 잔액 : " << balance << endl;
 	}
 	else cout << cnt << " 잔액이 없습니다.\n";
-	ready = false;
-	cv.notify_all();
-}
-
-void run() {
-	unique_lock<mutex> lck(mtx);
-	ready = false;
-	cv.notify_all();
+	mtx.unlock();
 }
 
 int main() {
-
-	int threadnum = 20;
-	std::thread threads[20];
-
-	for (int id = 0; id < threadnum; id++) {
+	thread threads[20];
+	for (int id = 0; id < 20; id++) {
 		if (id < 10) threads[id] = thread(depo, id);
 		else threads[id] = thread(draw, id);
 	}
 
-	run();
-
-	for (int id = 0; id < threadnum; id++)
-		threads[id].join();
-
-
+	for (int id = 0; id < 20; id++)	threads[id].join();
 	cout << "잔액 : " << balance << endl;
 }
